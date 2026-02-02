50b8563fd8ec2660a12ae55acbe7a754977cd291
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-02-01 18:06:52 -0800

    inital commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Import express
+import express from "express";
+
+// Create app
+const app = express();
+
+// Port
+const PORT = 3000;
+
+// Serve static files from public
+app.use(express.static("public"));
+
+// Home route
+app.get("/", (req, res) => {
+  res.redirect(`/home.html`);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

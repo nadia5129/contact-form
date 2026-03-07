558971f1a8f552ccee99b5fa8d74be6c0d2d5b1b
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-03-07 13:52:56 -0800

    connected mysql to contact form

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Import express
 import express from "express";
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create app
 const app = express();
@@ -16,9 +20,13 @@ app.set('view engine', 'ejs');
 //add middleware to allow express to read data thats in the form
 app.use(express.urlencoded({extended: true}));
 
-
-// create an array to store temp form submissions
-const contacts =[];
+// MySQL connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+}).promise();
 
 // Home route
 app.get("/", (req, res) => {
@@ -35,15 +43,21 @@ app.get('/confirmation', (req,res) => {
   res.render('confirmation');
 });
 
-//admin route that displays order arrays
-app.get('/admin', (req, res) => {
-  res.render('admin', {contacts});
+//admin route, reads all contacts from mysql
+app.get("/admin", async (req, res) => {
+  try {
+    const [contacts] = await pool.query("SELECT * FROM contacts ORDER BY created_at DESC");
+    res.render("admin", { contacts });
+  } catch (error) {
+    console.error("Error fetching contacts:", error);
+    res.status(500).send("Error loading admin page");
+  }
 });
 
 //submit route
-app.post('/confirmation', (req,res) => {
-  //create a json object to store order data
-  const Newcontact={
+app.post("/confirmation", async (req, res) => {
+try{
+  const newContact={
     fname: req.body.fname,
     lname: req.body.lname,
     email: req.body.email,
@@ -53,13 +67,34 @@ app.post('/confirmation', (req,res) => {
     meet: req.body.meet,
     other: req.body.other,
     message: req.body.message,
-    timestamp: new Date()
   };
+  
+   const sql = `
+      INSERT INTO contacts
+      (fname, lname, email, title, company, lurl, meet, other, message)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
 
-  contacts.push(Newcontact);
+     const values = [
+      newContact.fname,
+      newContact.lname,
+      newContact.email,
+      newContact.title,
+      newContact.company,
+      newContact.lurl,
+      newContact.meet,
+      newContact.other,
+      newContact.message
+    ];
 
-  res.render('confirmation', {contact: Newcontact});
 
+    await pool.query(sql, values);
+
+    res.render("confirmation", { contact: newContact });
+  } catch (error) {
+    console.error("Error saving contact:", error);
+    res.status(500).send("Error saving contact");
+  }
 });
 
 // Start server
